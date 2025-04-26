2d86969a9010578e2004ac4416ba02cdeea94e32
Robroi Neo M. Dingal <robroineo2@gmail.com>
2025-04-26 16:41:59 +0800

    add start window

--- app/gui/start.py
+++ app/gui/start.py
@@ -8,7 +8,7 @@ class startApp(QWidget):
         super().__init__()
         
         self.setWindowTitle("Algorithm Simulation")
-        self.resize(920, 720)
+        self.resize(1080, 350)
         
         #create objects
         self.label_1 = QLabel("Naive vs Fast-Expo")
@@ -17,20 +17,57 @@ class startApp(QWidget):
         self.button_1 = QPushButton("start")
         
         
-        font_db = QFontDatabase()
-        font_db.addApplicationFont("path/to/IBM-Plex-Mono-Regular.ttf")
-
         #stylesheets
-        self.setStyleSheet('background-color: #141920; color: #FFFFFF')
-        self.label_1.setStyleSheet('font-size: 64px; font-family: "Ibm Plex Mono", monospace;')
+        font_id = QFontDatabase.addApplicationFont("font/IBM_Plex_Mono/IBMPlexMono-Regular.ttf")
+        font_id_2 = QFontDatabase.addApplicationFont("font/Poppins/Poppins-Bold.ttf")
+            
+        if font_id == -1:
+            print("❌ Failed to load font") 
+        else:
+            font_1 = QFontDatabase.applicationFontFamilies(font_id)[0]
+            print(f"Loaded: {font_1}")
+            
+        if font_id_2 == -1:
+            print("❌ Failed to load font") 
+        else:
+            font_2 = QFontDatabase.applicationFontFamilies(font_id_2)[0]
+            print(f"Loaded: {font_2}")
         
+        self.setObjectName("MainWidget")        
+        self.setStyleSheet('#MainWidget{background-color: #141920; border: 2px solid #333; border-radius: 15px;}')
         
+        self.label_1.setStyleSheet(f'font-size: 64px; font-family:"{font_2}", sans-serif; color: white; font-weight: bold')
+        self.label_2.setStyleSheet(f'font-size: 32px; font-family:"{font_1}", monospace; color: #C7CBD7;')
+        self.label_2.setAlignment(Qt.AlignCenter)
+
+        
+        self.button_1.setStyleSheet("""
+                                    QPushButton {
+                                        background-color: #03CD97;
+                                        color: white;
+                                        font-size: 24px;
+                                        border: none;
+                                        border-radius: 10px;
+                                        padding: 10px 20px;
+                                    }
+                                    
+                                    QPushButton:hover {
+                                        background-color: #04B284;
+                                    }
+                                    
+                                    QPushButton:pressed {
+                                        background-color: #028E69;
+                                    }
+                                    """)
         #Layout
         master_layout = QVBoxLayout()
         master_layout.setAlignment(Qt.AlignCenter)
         
-        master_layout.addWidget(self.label_1)
-        master_layout.addWidget(self.label_2)
+        layout_1 = QVBoxLayout()        
+        layout_1.addWidget(self.label_1)
+        layout_1.addWidget(self.label_2)
+        
+        master_layout.addLayout(layout_1)
         master_layout.addWidget(self.button_1)
         
         self.setLayout(master_layout) 
