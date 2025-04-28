48d6af41a0df6ff362b4685de7d5e8b451d2da70
Robroi Neo M. Dingal <robroineo2@gmail.com>
2025-04-28 15:05:48 +0800

    add icon to button

--- app/windows/simulation_window.py
+++ app/windows/simulation_window.py
@@ -1,6 +1,7 @@
-from PyQt5.QtCore import Qt
+import os
+from PyQt5.QtCore import Qt, QSize
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout
-from PyQt5.QtGui import QFontDatabase
+from PyQt5.QtGui import QFontDatabase, QIcon
 
 class SimulationWindow(QWidget):
     
@@ -9,16 +10,30 @@ class SimulationWindow(QWidget):
         self.setWindowTitle("Simulation")
         self.resize(1280, 720)
         #objects
-        self.button = QPushButton("Back")
-        #stylesheets
+        self.back_btn = QPushButton("Back")
+        
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        icon_path = os.path.normpath(os.path.join(current_dir, '..', '..', 'icons', 'arrow_left.svg'))
+
+        self.back_btn.setIcon(QIcon(icon_path))
+        
+        #stylesheets11
         
         #layout
         master_layout = QVBoxLayout()
         master_layout.setAlignment(Qt.AlignCenter)
         
-        row_1 = QHBoxLayout()
-        row_2 = QHBoxLayout()
-        row_3 = QHBoxLayout()
+        self.row_1 = QHBoxLayout()
+        self.row_1.addWidget(self.back_btn)
+        
+        self.row_2 = QHBoxLayout()
+        self.row_3 = QHBoxLayout()
+        
+        master_layout.addLayout(self.row_1)
+        master_layout.addLayout(self.row_2)
+        master_layout.addLayout(self.row_3)
+        
+        self.setLayout(master_layout)
         
 def main():
     app = QApplication([])
